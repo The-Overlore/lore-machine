34ff224b595bf5800ebd06b7a732528ded29d40c
Constantin Wastchenko <80072466+cwastche@users.noreply.github.com>
2024-01-19 11:54:28 +0000

    Fixed issues w/ OpenAI gpt Interface.  (#25)
    * Fixed issues w/ OpenAI gpt Interface.
    
    * Small changes to pass tests after updates
    
    * Hello tests :)

--- overlore/types.py
+++ overlore/types.py
@@ -6,3 +6,7 @@ EventKeys: TypeAlias = list[str]
 EventData: TypeAlias = list[str]
 ToriiEvent: TypeAlias = dict[str, dict[str, str | EventKeys | EventData]]
 ParsedEvent: TypeAlias = dict[str, int | RealmPosition | ResourceAmounts | AttackingEntityIds | float]
+
+
+RealmData: TypeAlias = dict[str, str | int | list[int]]
+TownhallData: TypeAlias = dict[str, str | RealmData]
