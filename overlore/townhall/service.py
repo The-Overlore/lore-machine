5bacd7fb515e3b86b91d2e88cd108da5320df727
Johann <johann.bestowrous@gmail.com>
2024-01-22 06:42:43 -0800

    remove debugging deets

--- overlore/townhall/service.py
+++ overlore/townhall/service.py
@@ -5,7 +5,6 @@ import os
 import signal
 from types import FrameType
 
-import sqlean
 from dotenv import load_dotenv
 from websockets import WebSocketServerProtocol, serve
 
@@ -53,19 +52,17 @@ async def start() -> None:
     overlore_pulse = serve(bound_handler, args.address, SERVICE_WS_PORT)
 
     print(f"great job, starting this service on port {SERVICE_WS_PORT}. everything is perfect from now on.")
-
-    sqlean.connect(args.world_db)
-
+    process_event_bound_handler = functools.partial(process_event, extra_argument=events_db)
     await asyncio.gather(
         overlore_pulse,
         torii_event_sub(TORII_WS, process_event_bound_handler, Subscriptions.COMBAT_OUTCOME_EVENT_EMITTED),
         torii_event_sub(TORII_WS, process_event_bound_handler, Subscriptions.ORDER_ACCEPTED_EVENT_EMITTED),
     )
 
+
 # hardcode for now, when more mature we need some plumbing to read this off a config
 def main() -> None:
     asyncio.run(start())
-    print("wtf")
 
 
 if __name__ == "__main__":
