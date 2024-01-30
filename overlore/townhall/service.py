a274a72c818e50e61e02a31e2660ad6347cb3da4
cwastche <wastchenko.constantin@gmail.com>
2024-01-30 15:02:46 +0000

    Handle graceful shutdown when receiving SIGINT. resolves #31

--- overlore/townhall/service.py
+++ overlore/townhall/service.py
@@ -15,18 +15,22 @@ from overlore.sqlite.events_db import EventsDatabase
 from overlore.sqlite.vector_db import VectorDatabase
 from overlore.townhall.logic import handle_townhall_request
 
+global_shutdown_event = asyncio.Event()
 
-def handle_sigint(_signum: int, _b: FrameType | None) -> None:
-    exit(0)
+
+def handle_sigint(_signum: int, _frame: FrameType | None) -> None:
+    print("\nShutting down, wait for a few seconds...")
+    global_shutdown_event.set()
 
 
 async def service(websocket: WebSocketServerProtocol, config: Config) -> None:
-    async for message in websocket:
-        if message is None:
-            continue
-        print("generating townhall")
-        response = await handle_townhall_request(str(message), config)
-        await websocket.send(json.dumps(response))
+    while not global_shutdown_event.is_set():
+        async for message in websocket:
+            if message is None:
+                continue
+            print("generating townhall")
+            response = await handle_townhall_request(str(message), config)
+            await websocket.send(json.dumps(response))
 
 
 async def start() -> None:
@@ -43,17 +47,24 @@ async def start() -> None:
     OpenAIHandler.instance().init(config.OPENAI_API_KEY)
 
     service_bound_handler = functools.partial(service, config=config)
-    overlore_pulse = serve(service_bound_handler, config.address, config.port)
+    overlore_pulse = await serve(service_bound_handler, config.address, config.port)
+    combat_sub_task = asyncio.create_task(
+        torii_event_sub(config.TORII_WS, process_event, Subscriptions.COMBAT_OUTCOME_EVENT_EMITTED)
+    )
+    order_sub_task = asyncio.create_task(
+        torii_event_sub(config.TORII_WS, process_event, Subscriptions.ORDER_ACCEPTED_EVENT_EMITTED)
+    )
 
     print(f"great job, starting this service on port {config.port}. everything is perfect from now on.")
     try:
-        await asyncio.gather(
-            overlore_pulse,
-            torii_event_sub(config.TORII_WS, process_event, Subscriptions.COMBAT_OUTCOME_EVENT_EMITTED),
-            torii_event_sub(config.TORII_WS, process_event, Subscriptions.ORDER_ACCEPTED_EVENT_EMITTED),
-        )
+        await global_shutdown_event.wait()
     except ConnectionClosedError:
         print("Connection close on Torii, need to reconnect here")
+    finally:
+        combat_sub_task.cancel()
+        order_sub_task.cancel()
+        overlore_pulse.close()
+        await overlore_pulse.wait_closed()
 
 
 # hardcode for now, when more mature we need some plumbing to read this off a config
